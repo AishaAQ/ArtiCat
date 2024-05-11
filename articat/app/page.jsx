ef5764565468c164caaef6dd420ed55f6da906cd
AishaAQ <101647309+AishaAQ@users.noreply.github.com>
2024-05-11 14:58:20 +0300

    Revert "Update page.jsx"
    This reverts commit bd37f222c0e60bcfe1464d5b8bdabbb0a6f767e7.

--- articat/app/page.jsx
+++ articat/app/page.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 
 export default function Home() {
   const router = useRouter()
-  useEffect(() => {router.push('/mainpage.html')},[])
   return (
-    <></>
+    router.push('/mainpage.html')
   )
 }
