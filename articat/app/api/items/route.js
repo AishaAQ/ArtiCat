f4b2eaf07a98e6b3be0ce4518cf3d48c8bbcfa12
AishaAQ <101647309+AishaAQ@users.noreply.github.com>
2024-05-11 06:58:44 +0300

    Update route.js

--- articat/app/api/items/route.js
+++ articat/app/api/items/route.js
@@ -2,27 +2,24 @@ import articatRepo from "@/app/repo/articat-repo"
 const ArtiCatRepo = new articatRepo()
 
 export async function GET(request) {
-    // let items
-    // const {searchParams} = new URL(request.url)
-    // const category = searchParams.get('category')
-    // const searchValue = searchParams.get('searchValue')
-    // const featured = searchParams.get('featured')
+    let items
+    const {searchParams} = new URL(request.url)
+    const category = searchParams.get('category')
+    const searchValue = searchParams.get('searchValue')
+    const featured = searchParams.get('featured')
     
-    // if (category) {
-    //     items = await ArtiCatRepo.getItems(category)
-    // } else if (searchValue) {
-    //     items = await ArtiCatRepo.getSearchItems(searchValue)
-    // } else if (featured) {
-    //     items = await ArtiCatRepo.getFeatured()
-    // } else {
-    //     items = await ArtiCatRepo.getAllItems()
-    // }
+    if (category) {
+        items = await ArtiCatRepo.getItems(category)
+    } else if (searchValue) {
+        items = await ArtiCatRepo.getSearchItems(searchValue)
+    } else if (featured) {
+        items = await ArtiCatRepo.getFeatured()
+    } else {
+        items = await ArtiCatRepo.getAllItems()
+    }
 
    
-    // return new Response(JSON.stringify(items), {
-    //     status: 200
-    // })
-    return new Response("check", {
+    return new Response(JSON.stringify(items), {
         status: 200
     })
 }
