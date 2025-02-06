e6ce42688d5690555e22dbfc69c0b8aaca22fe05
Aisha <aisha.abdulquadirzaifullah@gmail.com>
2025-02-06 03:52:17 +0300

    Update articat-repo.js

--- articat/app/repo/articat-repo.js
+++ articat/app/repo/articat-repo.js
@@ -1,6 +1,6 @@
 // import { PrismaClient } from '@prisma/client'
 // const prisma = new PrismaClient()
-import prisma from '../lib/prisma.js';
+import prisma from '@/lib/prisma'
 
 export default class articatRepo {
 
